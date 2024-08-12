24a8d3d547fb76c980d500f98f1879a1f89e5e09
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-08-12 13:39:22 +0530

    Add files via upload

---
+++ week1/day1/Browser.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		
+		//dataType variableName= value
+		//variable name which given inside main method- loval variable
+		//variable -camel case
+ int noOfTabOpen=3;
+ String searchBox="Selenium";
+ boolean tabsClose=true;
+ char browserLog='c';
+ float version=121.34f;
+ long ph=45678990098765L;
+ 
+ System.out.println("number of tabs open :"+noOfTabOpen);
+ System.out.println("print search value :"+searchBox);
+ System.out.println("Check current tab is closed :"+tabsClose
+		 +"\n"+"show the bowser Log :"+browserLog
+		 +"\n"+"print the browser version :"+version);
+	}
+
+}
---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	//main-> ctrl + space
+	public static void main(String[] args) {
+		
+		//syso->ctrl+space
+		
+		System.out.println("Welcome to TestLeaf");
+        System.out.println("Selenium WebDriver August 2024 batch");	
+	}
+
+}
